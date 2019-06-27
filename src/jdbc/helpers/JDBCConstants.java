ce58e92584dea192b1360b8d2f4343737c0893e0
Mark Rai <markraidc@gmail.com>
2019-06-26 21:32:25 -0400

    fixed Oracle driver name.

--- src/jdbc/helpers/JDBCConstants.java
+++ src/jdbc/helpers/JDBCConstants.java
@@ -8,7 +8,7 @@ package jdbc.helpers;
 public class JDBCConstants
 {
 
-   public static final String DRIVER_NAME = "org.hsqldb.jdbc.JDBCDriver";
+   public static final String DRIVER_NAME = "oracle.jdbc.OracleDriver";
    public static final String URL         = "jdbc:oracle:thin:@localhost:1521:xe";
    public static final String USERNAME    = "SYSTEM";
    public static final String PASSWORD    = "0000";
