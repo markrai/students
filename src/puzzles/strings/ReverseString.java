920cbb005036229da4d8d7f475d488e47c990b6a
Mark Rai <markraidc@gmail.com>
2019-08-09 09:32:04 -0400

    Reverse String (completed)

--- src/puzzles/strings/ReverseString.java
+++ src/puzzles/strings/ReverseString.java
@@ -1,9 +1,12 @@
 package puzzles.strings;
 
+import java.util.stream.Stream;
+
 public class ReverseString {
 
 	static String input1 = "Hello World";
 	static String input2 = "Hello World";
+//	static String input3 = "Hello World";
 
 	// with StringBuilder
 	public void withStringBuilder(String input1) {
@@ -12,28 +15,39 @@ public class ReverseString {
 		System.out.println(sb1.reverse().toString());
 	}
 
-	
 	// using StringBuilder only to store mutable String
-	public void withoutStringBuilder(String input2) {
+	public void leveragingStringBuilder(String input2) {
 
 		StringBuilder sb2 = new StringBuilder();
-		
-		for (int i = sb2.length() - 1; i >= 0; i--) {
+
+		for (int i = input2.length() - 1; i >= 0; i--) {
 			sb2.append(input2.charAt(i));
 		}
 
 		System.out.println(sb2.toString());
 	}
 
+/*	public void withoutStringBuilder(String input3) {
+
+		String thirdString;
+
+				}
+		}
+
+	}
+*/
 	public static void main(String[] args) {
 
 		ReverseString r = new ReverseString();
 
 		System.out.print("Using String Builder's reverse() method: ");
 		r.withStringBuilder(input1);
-		
+
 		System.out.print("Using String Builder only to store the String: ");
-		r.withoutStringBuilder(input2);
+		r.leveragingStringBuilder(input2);
+
+	//	System.out.print("Using just a String: ");
+	//	r.withoutStringBuilder(input3);
 
 	}
 
