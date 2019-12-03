030260cfbcdbd5344fc0286aa79e3569b25c12d8
Mark Rai <markraidc@gmail.com>
2019-12-03 14:01:25 -0500

    routine

--- src/days/day08/AnonymousClass/Usecase.java
+++ src/days/day08/AnonymousClass/Usecase.java
@@ -18,7 +18,7 @@ class B {
 
 public class Usecase {
 	public static void main(String[] args) {
-		B obj = new B();
+		A obj = new A();
 		obj.show();
 	}
 }
--- src/days/day08/lambda/basicexample/BasicComparatorExample.java
+++ src/days/day08/lambda/basicexample/BasicComparatorExample.java
@@ -5,23 +5,25 @@ import java.util.Comparator;
 public class BasicComparatorExample {
 
 	static String a = "abcdefgh";
-	static String b = "ijklmnop";
-	
-	
+	static String b = "ijklmnopa";
+
 	public static void main(String[] args) {
 
 		Comparator<String> comparatorObject = new Comparator<String>() {
 
 			public int compare(String s1, String s2) {
 
+				System.out.println(Integer.compare(s1.length(), s2.length()));
+				
 				return Integer.compare(s1.length(), s2.length());
 
 			}
 
 		};
-		
-		// somehow use this comparator object to do great things...
-		comparatorObject.compare(a, b);
+
+		// use this comparator object to do something with it.
+		//someMethod(data, comparatorObject);
+
 	}
 
 }
